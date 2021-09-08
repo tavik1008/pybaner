35a0edb761e5673588754429873d4a62545b281e
Milky <76628570+tavik1008@users.noreply.github.com>
2021-09-09 00:33:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ async def banner():
         id = 693835534694350888
         draw.text((740, 380), f"{str(members)}", font=fnt_1)
         fnt_2 = ImageFont.truetype("DrukCyr-Super.ttf", size=90)
-        draw.text((615, 270), str(bot.get_guild(id).member_count), font=fnt_2)
+        draw.text((235, 387), str(bot.get_guild(id).member_count), font=fnt_2)
         fnt_3 = ImageFont.truetype("DrukCyr-Super.ttf", size=160)
         draw.text((1010, 1010), str(bot.get_guild(id).premium_subscription_count), font=fnt_3)
         return await bot.get_guild(id).edit(banner=get_bio_from_image(img))
