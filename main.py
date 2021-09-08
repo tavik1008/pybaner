fd57da9fa3c3b3cd24ec53bd0b1508f40c5ac164
Milky <76628570+tavik1008@users.noreply.github.com>
2021-09-09 00:35:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from PIL import Image, ImageFilter, ImageDraw, ImageFont, ImageOps, ImageColor
 from io import BytesIO
 import io
 import os
-bot = commands.Bot(command_prefix='.')
+
+bot=commands.Bot(command_prefix='.')
 bot.remove_command('help')
-@tasks.loop(seconds=3)
+
+@tasks.loop(seconds = 3)
 async def banner():
     id = 693835534694350888
     members = sum([len(ch.members) for ch in bot.get_guild(id).voice_channels])
@@ -14,19 +16,21 @@ async def banner():
         draw = ImageDraw.Draw(img)
         fnt_1 = ImageFont.truetype("DrukCyr-Super.ttf", size=90)
         id = 693835534694350888
-        draw.text((740, 380), f"{str(members)}", font=fnt_1)
+        draw.text((557, 389), f"{str(members)}", font=fnt_1)
         fnt_2 = ImageFont.truetype("DrukCyr-Super.ttf", size=90)
-        draw.text((235, 387), str(bot.get_guild(id).member_count), font=fnt_2)
+        draw.text((233, 390), str(bot.get_guild(id).member_count), font=fnt_2)
         fnt_3 = ImageFont.truetype("DrukCyr-Super.ttf", size=160)
-        draw.text((1010, 1010), str(bot.get_guild(id).premium_subscription_count), font=fnt_3)
+        draw.text((1000, 1000), str(bot.get_guild(id).premium_subscription_count), font=fnt_3)
         return await bot.get_guild(id).edit(banner=get_bio_from_image(img))
+    
 def get_bio_from_image(img):
     bio = io.BytesIO()
     bio.name = 'image.png'
     img.save(bio, 'png')
     return bio.getvalue()
+
 @bot.event
 async def on_ready():
     banner.start()
-     
-bot.run('ODc1Njk1NTkzMDEwMzg5MDMy.YRZREw.H6gAIfd6TPXJ1ag_PQoqa7ikUh8')
+
+bot.run('NzMzOTgxMzA4MDE5MDE1NzEw.XxLDXA.TSFC-M3w2s32pSG9EB2ImxBTqc0')
